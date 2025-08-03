f7a0d5955d76315b7b77b3927a772af22d694e36
TERMOPALE\anton <anton.shheglov.1@gmail.com>
2025-08-03 23:57:09 +0300

    Add Microkernel as a subtree / Chore

--- BaseApplication/PluginLoader/PluginsWatcher/IPluginsWatcher.cs
+++
@@ -1,8 +0,0 @@
-namespace PluginLoader.PluginsWatcher;
-
-public interface IPluginsWatcher : IDisposable
-{
-	void RegisterOnFileDelete(Action<string> onDeleteAction);
-	void RegisterOnFileAdd(Action<string> onAddAction);
-	void StartWatching();
-}
